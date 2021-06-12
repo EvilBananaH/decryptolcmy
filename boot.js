48143ad15f9608acd13a2b1fb464b7f57764047c
EvilBananaH <danik5451@gmail.com>
2021-06-13 01:49:31 +0300

    Add files via upload

---
+++ boot.js
@@ -0,0 +1,62 @@
+var cluster = require('cluster');
+var path = require('path');
+var workerCount = 16;
+
+
+cluster.setupMaster({ exec: path.join(__dirname, './subs/server.js') });
+
+function numWorkers() { return Object.keys(cluster.workers).length; }
+
+var stopping = false;
+
+function forkNewWorkers() {
+  if (!stopping) {
+    for (var i = numWorkers(); i < workerCount; i++) { cluster.fork(); }
+  }
+}
+
+var workersToStop = [];
+
+
+function stopWorker(worker) {
+  console.log('stopping', worker.process.pid);
+  worker.disconnect();
+  var killTimer = setTimeout(function() {
+    worker.kill();
+  }, 60000);
+
+  
+  killTimer.unref();
+}
+
+
+function stopNextWorker() {
+  var i = workersToStop.pop();
+  var worker = cluster.workers[i];
+  if (worker) stopWorker(worker);
+}
+
+function stopAllWorkers() {
+  stopping = true;
+  console.log('stopping all workers');
+  for (var id in cluster.workers) {
+    stopWorker(cluster.workers[id]);
+  }
+}
+
+
+cluster.on('listening', stopNextWorker);
+
+
+cluster.on('disconnect', forkNewWorkers);
+
+process.on('SIGHUP', function() {
+  console.log('restarting all workers');
+  workersToStop = Object.keys(cluster.workers);
+  stopNextWorker();
+});
+
+process.on('SIGTERM', stopAllWorkers);
+
+forkNewWorkers();
+console.log('[>] Script Running: master', process.pid, 'booted\n[>] Workers: ', workerCount);
